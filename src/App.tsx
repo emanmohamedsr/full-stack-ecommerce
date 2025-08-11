709f77f78c50160ff09844634974efe890740b26
Eman Mohamed <emanmohamedsr72@gmail.com>
2025-08-11 10:45:21 +0300

    install chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button, HStack } from "@chakra-ui/react";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<HStack>
+			<Button>Click me</Button>
+			<Button>Click me</Button>
+		</HStack>
+	);
 };
 
 export default App;
