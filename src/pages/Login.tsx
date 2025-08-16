28f015f47f8beb65721a39871b3cd4aafcc1cc62
Eman Mohamed <emanmohamedsr72@gmail.com>
2025-08-16 13:44:42 +0300

    create login, signup and logout pages full ui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,76 @@
+"use client";
+
+import { useColorMode } from "@/hooks/useColorMode";
+import {
+	Flex,
+	Input,
+	Fieldset,
+	Field,
+	Stack,
+	Button,
+	Heading,
+} from "@chakra-ui/react";
+
 const LoginPage = () => {
-	return <div>Login Page</div>;
+	const { colorMode } = useColorMode();
+	return (
+		<Flex
+			minH={"calc(100vh - 64px)"}
+			align={"center"}
+			justify={"center"}
+			bg={colorMode === "light" ? "white" : "gray.800"}>
+			<Stack mx={"auto"} maxW={"lg"} gap={6} spaceY={6} px={6}>
+				<Heading
+					fontSize={"4xl"}
+					color={colorMode === "light" ? "teal.600" : "teal.600"}>
+					Sign in to your account
+				</Heading>
+
+				<Fieldset.Root
+					size='lg'
+					maxW='md'
+					shadow={"md"}
+					bg={colorMode === "light" ? "gray.50" : "gray.700"}
+					p={6}
+					rounded='md'>
+					<Stack gap={4}>
+						<Fieldset.Legend>Contact details</Fieldset.Legend>
+						<Fieldset.HelperText>
+							Please provide your contact details below.
+						</Fieldset.HelperText>
+					</Stack>
+
+					<Fieldset.Content>
+						<Field.Root>
+							<Field.Label>Email address</Field.Label>
+							<Input name='email' type='email' />
+							<Field.HelperText color={"red.600"} fontSize={"sm"}>
+								<Field.ErrorIcon w={4} mr={2} />
+								error
+							</Field.HelperText>
+						</Field.Root>
+						<Field.Root>
+							<Field.Label>Password</Field.Label>
+							<Input name='password' type='password' />
+							<Field.HelperText color={"red.600"} fontSize={"sm"}>
+								<Field.ErrorIcon w={4} mr={2} />
+								error
+							</Field.HelperText>
+						</Field.Root>
+					</Fieldset.Content>
+
+					<Button
+						type='submit'
+						alignSelf='flex-start'
+						mt={5}
+						bg='teal.700'
+						color='white'>
+						Submit
+					</Button>
+				</Fieldset.Root>
+			</Stack>
+		</Flex>
+	);
 };
 
 export default LoginPage;
--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,5 +1,52 @@
+import { useColorMode } from "@/hooks/useColorMode";
+import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
 const LogoutPage = () => {
-	return <div>Logout Page</div>;
+	const navigate = useNavigate();
+	const { colorMode } = useColorMode();
+	return (
+		<Flex
+			minH={"calc(100vh - 64px)"}
+			align={"center"}
+			justify={"center"}
+			bg={colorMode === "light" ? "white" : "gray.800"}>
+			<Stack mx={"auto"} maxW={"lg"} gap={6} spaceY={6} px={6}>
+				<Heading
+					fontSize={"4xl"}
+					color={colorMode === "light" ? "teal.600" : "teal.600"}
+					textAlign={"center"}>
+					Logout
+				</Heading>
+				<Box
+					maxW='md'
+					shadow={"md"}
+					bg={colorMode === "light" ? "gray.50" : "gray.700"}
+					p={6}
+					rounded='md'>
+					<Text
+						color={colorMode === "light" ? "gray.700" : "gray.200"}
+						fontSize='lg'
+						textAlign='center'>
+						We hope to see you again soon! If you have any feedback or
+						suggestions, please let us know.
+					</Text>
+					<Stack gap={4} mt={6}>
+						<Button variant='solid' bg='red.700' color='white'>
+							Logout
+						</Button>
+						<Button
+							variant='solid'
+							bg='teal.700'
+							color='white'
+							onClick={() => navigate(-1)}>
+							Back
+						</Button>
+					</Stack>
+				</Box>
+			</Stack>
+		</Flex>
+	);
 };
 
 export default LogoutPage;
